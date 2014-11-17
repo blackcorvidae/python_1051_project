70fcec773b47e96d60d0fbaf102efce8658d9505
Mark Rose <blackcorvidae@gmail.com>
2014-11-17 11:04:32 -0500

    added database insert, and now we're going to make a better cli program

--- Extractor.py
+++ Extractor.py
@@ -1,10 +1,35 @@
 import urllib
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
+import datetime
 
 class Extractor:
     def __init__(self,base_url):
         self.base_url = base_url
+    def add_config(self, post):
+        client = MongoClient('mongodb://localhost:27017/')
+        db = client.dom_project
+        collection = db.options
+        post_id = collection.insert(post)
+        return post_id
+    collected_urls = []
+    def add_url_to_collected(self, url):
+        if not self.search_collected_urls(url):
+            self.collected_urls.append(url)
+    def get_collected_urls(self):
+        return self.collected_urls
+    def search_collected_urls(self, find_url):
+        for url in self.collected_urls:
+            if url == find_url:
+                return True
+            else:
+                return False
+    def iterate_hrefs_by_keyword(self, keyword, html_doc):
+        list_of_hrefs = self.find_keyword_in_href(keyword, html_doc)
+        if list_of_hrefs:
+            for href in list_of_hrefs:
+                #print href
+                self.add_url_to_collected(href)
     def get_html_from_url(self):
         # We want to use parameters eventually
         # params = urllib.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
--- url_extractor.py
+++ url_extractor.py
@@ -4,19 +4,31 @@ from bs4 import BeautifulSoup
 from Extractor import *
 
 url = 'http://www.crummy.com/software/BeautifulSoup/'
-
 extract = Extractor(url)
+
+
+#Config
+findthis = "www.crummy.com"
+post = {"base_url": url, "keyword": findthis, "updated": datetime.datetime.utcnow()}
+extract.add_config(post)
+
+
+
+
+#Reading
 html_object =  extract.get_html_from_url()
-
 #html_object =  getUrl(url)
-
 html_doc = html_object.read()
-
-findthis = "crummy"
-
 list_of_hrefs = extract.find_keyword_in_href(findthis, html_doc)
 
 
 if list_of_hrefs:
     for href in list_of_hrefs:
-        print href
+        #print href
+        extract.add_url_to_collected(href)
+
+collected_urls = extract.get_collected_urls()
+for url in collected_urls:
+    print url
+
+
